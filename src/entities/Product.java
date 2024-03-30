363352fda06bce4c2a93518f99295c350ee7a6e6
João Pedro <joaopedrov159@hotmail.com>
2024-03-30 11:04:05 -0300

    Implementation

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,5 +1,37 @@
 package entities;
 
-public class Product {
+public class Product implements Comparable<Product> {
+	private String name;
+	private Double price;
+	
+	public Product(String n, Double p) {
+		name = n;
+		price = p;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return name + ", " + String.format("%.2f", price);
+	}
+	
+	@Override
+	public int compareTo(Product product) {
+		return price.compareTo(product.getPrice());
+	}
 }
--- src/services/CalculationService.java
+++ src/services/CalculationService.java
@@ -1,5 +1,20 @@
 package services;
 
-public class CalculationService {
+import java.util.List;
 
+public class CalculationService {
+	public static <T extends Comparable<T>> T max(List<T> list) {
+		if(list.isEmpty()) {
+			throw new IllegalStateException("List can't be empty!");
+		}
+		
+		T max = list.get(0);
+		
+		for(T l : list) {
+			if(l.compareTo(max) > 0) {
+				max = l;
+			}
+		}
+		return max;
+	}
 }
